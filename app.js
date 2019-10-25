c6da6a82eac5c20bf06ba2b1faa3ef4aadb544c6
Andrew Hughes <Andrew.Hughes1@ibm.com>
2019-10-25 10:52:51 +0100

    license and fixup

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ const app = require('express')()
 const server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.on('disconnect', function(){
+  socket.on('disconnect', () => {
     console.log('user disconnected');
   });
-  socket.on('chat message', function(msg){
+  socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
 });
 
-server.listen(3000, function(){
+server.listen(3000, () => {
   console.log('listening on *:3000');
 });
