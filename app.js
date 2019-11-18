282d94e6e2600e1cb84517360cca241f921521a2
Andrew Hughes <andrew.hughes.101@outlook.com>
2019-11-18 10:41:11 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const app = require('express')()
 const server = require('http').createServer(app);
-var io = require('socket.io')(server);
+const io = require('socket.io')(server);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
