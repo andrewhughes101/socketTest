554f0762c755e2b2bbd175611157bf94b9aa3df6
Andrew Hughes <Andrew.Hughes1@ibm.com>
2019-11-18 15:01:11 +0000

    updates: using sams stack

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-const app = require('express')()
-const server = require('http').createServer(app);
-var io = require('socket.io')(server);
+module.exports = () => {
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+  const app = require('express')()
+  const server = require('http').createServer(app);
+  const io = require('socket.io')(server)
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/index.html');
+  })
+
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
   });
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-});
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+  
+  return app, server;
+}
